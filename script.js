01f7342ffd8392d3153102b650422a3fddeaabb8
Niranjan V Ram <niranjanvram@gmail.com>
2024-10-08 22:28:06 +0530

    - close navbar before leaving a page

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-	var toggleButton = document.querySelector('.navbar-toggle');
-	var navbarFull = document.querySelector('.navbar-fullscreen');
+    var toggleButton = document.querySelector('.navbar-toggle');
+    var navbarFull = document.querySelector('.navbar-fullscreen');
   
-	toggleButton.addEventListener('click', function () {
-	  toggleButton.classList.toggle('active');
-	  if (navbarFull.classList.contains('active')) {
-		
-		navbarFull.classList.remove('active');
-		setTimeout(function () {
-		  navbarFull.style.display = 'none';
-		}, 600);
-	  }
-	  else {
-		navbarFull.style.display = 'block';
-		setTimeout(function () {
-		  navbarFull.classList.add('active');
-		}, 10);
-	  }
-	});
-  });
-  
+    toggleButton.addEventListener('click', function () {
+        toggleButton.classList.toggle('active');
+        if (navbarFull.classList.contains('active')) {
+            navbarFull.classList.remove('active');
+            setTimeout(function () {
+                navbarFull.style.display = 'none';
+            }, 600);
+        } else {
+            navbarFull.style.display = 'block';
+            setTimeout(function () {
+                navbarFull.classList.add('active');
+            }, 10);
+        }
+    });
+
+    window.addEventListener('beforeunload', function () {
+        if (navbarFull.classList.contains('active')) {
+            navbarFull.classList.remove('active');
+            navbarFull.style.display = 'none';
+        }
+    });
+});
