e514b6ed883759158352108bbe6775ff55cf134b
Niranjan V Ram <niranjanvram@gmail.com>
2024-10-24 12:37:02 +0530

    - website now follows only dark theme - the navbar has a new look - minor fixes

--- functions.js
+++ functions.js
@@ -12,35 +12,6 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
-// Function to switch themes
-function switchTheme() {
-    const currentTheme = document.documentElement.getAttribute('data-theme');
-
-    if (currentTheme === 'light') {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-    }
-}
-
-// Function to apply the saved theme or default to dark
-function applySavedTheme() {
-    const savedTheme = localStorage.getItem('theme') || 'dark';
-    document.documentElement.setAttribute('data-theme', savedTheme);
-}
-
-// Listen for the 'd' key press to toggle the theme
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'd') {
-        switchTheme();
-    }
-});
-
-// Apply the saved theme on page load
-applySavedTheme();
-
 
 document.addEventListener("DOMContentLoaded", function () {
     // Remove fade-out class on page load or back navigation
