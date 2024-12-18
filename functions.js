ab57558453ac702193dc9282b925a1bd2d03d634
Niranjan V Ram <niranjanvram@gmail.com>
2024-12-18 23:49:24 +0530

    - added new themes

--- functions.js
+++ functions.js
@@ -63,7 +63,8 @@ document.addEventListener('keydown', function(event) {
 
 //change theme on 'D' or 'd' key press
 document.addEventListener('DOMContentLoaded', function () {
-	const themes = ['dark-theme', 'light-theme', 'blue-n-blue', 'fruity', 'forest'];
+	//const themes = ['dark-theme', 'light-theme', 'blue-n-blue', 'fruity', 'forest'];
+	const themes = ['monochrome-elegance', 'forest-night', 'lavender-mist', 'charcoal-lime', 'arctic-horizon'];
 
 	// Function to update active theme dot
 	function updateActiveDot(theme) {
